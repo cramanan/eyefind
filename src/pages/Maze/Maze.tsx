55a9ec5cf1ce948ddbfc225b608be364ef637f8a
cramanan <cramananjaonapro@gmail.com>
2024-09-11 09:12:39 +0200

    feat: translated project to NUI Boilerplate

---
+++ src/pages/Maze/Maze.tsx
@@ -0,0 +1,121 @@
+import { Link, Outlet } from "react-router-dom";
+import logo from "../../assets/Maze.webp";
+import "./Maze.scss";
+import { createContext, useContext, useEffect, useState } from "react";
+import { fetchNui } from "../../utils/fetchNui";
+
+interface User {
+    identifier: string;
+    balance: number;
+}
+
+interface Initial {
+    user: User | null;
+    setUser: (u: User | null) => void;
+}
+
+const context = createContext<Initial>({ user: null, setUser: console.log });
+
+export function Maze() {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    const fetchUser = () => {
+        fetchNui<User | null>("auth", null, null)
+            .then((user) => {
+                setUser(user);
+                setLoading(false);
+            })
+            .catch(console.error);
+    };
+
+    useEffect(fetchUser, []);
+
+    return (
+        <context.Provider value={{ user, setUser }}>
+            <div id="background">
+                <span id="bar"></span>
+                <div id="container">
+                    <Link to="">
+                        <img src={logo} alt="Maze Bank Logo" id="logo" />
+                    </Link>
+                    <span id="sep"></span>
+                    <div id="outlet">
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </context.Provider>
+    );
+}
+
+export function MazeHome() {
+    const { user } = useContext(context);
+    return (
+        <div id="home">
+            <div id="choices">
+                <div id="choice">
+                    {!user ? (
+                        <Link to="open-account" className="red-button">
+                            Open a bank account
+                        </Link>
+                    ) : (
+                        <>
+                            <Link to="deposit" className="red-button">
+                                Deposit
+                            </Link>
+                            <Link to="withdraw" className="red-button">
+                                Withdraw
+                            </Link>
+                            <Link to="logs" className="red-button">
+                                Transaction Logs
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function OpenAccount() {
+    const { user, setUser } = useContext(context);
+    const [loading, setLoading] = useState(true);
+
+    const createAccount = () => {
+        fetchNui<User | null>("create", null, {
+            identifier: "fivem:identifier",
+            balance: 5000,
+        })
+            .then((user) =>
+                setTimeout(() => {
+                    setLoading(false);
+                    setUser(user);
+                }, 2000)
+            )
+            .catch(console.error);
+    };
+
+    if (!user) createAccount();
+
+    if (user) return <>You already own a banking account</>;
+
+    if (loading) return <>Creating account...</>;
+
+    return <>Account created successfully</>;
+}
+export function MazeDeposit() {
+    const user = useContext(context);
+    if (!user) return <>You do not own any banking account</>;
+    return <>Deposit Loading</>;
+}
+
+export function MazeWithdraw() {
+    const user = useContext(context);
+    if (!user) return <>You do not own any banking account</>;
+    return <>Withdraw Loading</>;
+}
+
+export function Maze404() {
+    return <>404</>;
+}
---
+++ src/providers/VisibilityProvider.tsx
@@ -0,0 +1,59 @@
+import {
+    Context,
+    createContext,
+    ReactNode,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
+import { useNuiEvent } from "../hooks/useNuiEvent";
+import { fetchNui } from "../utils/fetchNui";
+import { isEnvBrowser } from "../utils/misc";
+
+const VisibilityCtx = createContext<VisibilityProviderValue | null>(null);
+
+interface VisibilityProviderValue {
+    setVisible: (visible: boolean) => void;
+    visible: boolean;
+}
+
+// This should be mounted at the top level of your application, it is currently set to
+// apply a CSS visibility value. If this is non-performant, this should be customized.
+export function VisibilityProvider({ children }: { children: ReactNode }) {
+    const [visible, setVisible] = useState(isEnvBrowser());
+
+    useNuiEvent<boolean>("setVisible", setVisible);
+
+    // Handle pressing escape/backspace
+    useEffect(() => {
+        // Only attach listener when we are visible
+        if (!visible) return;
+
+        const keyHandler = (e: KeyboardEvent) => {
+            if (["Backspace", "Escape"].includes(e.code)) {
+                if (!isEnvBrowser()) fetchNui("hideFrame");
+                else setVisible(!visible);
+            }
+        };
+
+        window.addEventListener("keydown", keyHandler);
+
+        return () => window.removeEventListener("keydown", keyHandler);
+    }, [visible]);
+
+    return (
+        <VisibilityCtx.Provider
+            value={{
+                visible,
+                setVisible,
+            }}
+        >
+            {visible ? children : <></>}
+        </VisibilityCtx.Provider>
+    );
+}
+
+export const useVisibility = () =>
+    useContext<VisibilityProviderValue>(
+        VisibilityCtx as Context<VisibilityProviderValue>
+    );
